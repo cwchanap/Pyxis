414bf9a836ea02aae4a68cf9783fc6181232a2f2
cwchanap <cwchanap@connect.ust.hk>
2026-05-06 12:31:34 -0700

    Add idle kingdom launch smoke test

--- PyxisUITests/PyxisUITests.swift
+++ PyxisUITests/PyxisUITests.swift
@@ -23,14 +23,11 @@ final class PyxisUITests: XCTestCase {
     }
 
     @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testLaunchesGameScene() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // XCUIAutomation Documentation
-        // https://developer.apple.com/documentation/xcuiautomation
+        XCTAssertEqual(app.state, .runningForeground)
     }
 
     @MainActor
