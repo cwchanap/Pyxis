c5ad7d13fbf26c4af688d35443b7406aca229264
cwchanap <cwchanap@connect.ust.hk>
2026-05-06 12:16:47 -0700

    Build idle kingdom SpriteKit scene

--- Pyxis/GameScene.swift
+++ Pyxis/GameScene.swift
@@ -6,83 +6,293 @@
 //
 
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+final class GameScene: SKScene {
+    private enum ButtonName {
+        static let spawn = "spawnSoldierButton"
+        static let upgrade = "upgradeSoldierButton"
+    }
+
+    private let store: KingdomGameStore
+    private var state: KingdomGameState
+    private var didBuildInterface = false
+
+    private let goldLabel = SKLabelNode(fontNamed: "AvenirNext-DemiBold")
+    private let cityLevelLabel = SKLabelNode(fontNamed: "AvenirNext-DemiBold")
+    private let soldierAttackLabel = SKLabelNode(fontNamed: "AvenirNext-Medium")
+    private let cityHPLabel = SKLabelNode(fontNamed: "AvenirNext-Medium")
+    private let feedbackLabel = SKLabelNode(fontNamed: "AvenirNext-Medium")
+    private let hpBarBackground = SKShapeNode()
+    private let hpBarFill = SKShapeNode()
+    private let spawnButton = SKNode()
+    private let spawnButtonBackground = SKShapeNode()
+    private let spawnButtonLabel = SKLabelNode(fontNamed: "AvenirNext-DemiBold")
+    private let upgradeButton = SKNode()
+    private let upgradeButtonBackground = SKShapeNode()
+    private let upgradeButtonLabel = SKLabelNode(fontNamed: "AvenirNext-DemiBold")
+
+    private var feedbackText = "Tap Spawn Soldier to attack the city."
+
+    init(size: CGSize, store: KingdomGameStore = .shared) {
+        self.store = store
+        self.state = store.load()
+        super.init(size: size)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        self.store = .shared
+        self.state = KingdomGameStore.shared.load()
+        super.init(coder: aDecoder)
+    }
+
     override func didMove(to view: SKView) {
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        backgroundColor = SKColor(red: 0.07, green: 0.10, blue: 0.13, alpha: 1.0)
+
+        if !didBuildInterface {
+            buildInterface()
+            didBuildInterface = true
         }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+
+        redraw()
+        layoutInterface()
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+
+    override func didChangeSize(_ oldSize: CGSize) {
+        super.didChangeSize(oldSize)
+        layoutInterface()
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
+
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+        guard let touch = touches.first else {
+            return
+        }
+
+        switch buttonName(at: touch.location(in: self)) {
+        case ButtonName.spawn:
+            spawnSoldier()
+        case ButtonName.upgrade:
+            upgradeSoldier()
+        default:
+            break
+        }
     }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+
+    private func buildInterface() {
+        configureLabel(goldLabel, fontSize: 28, color: SKColor(red: 1.0, green: 0.84, blue: 0.25, alpha: 1.0))
+        configureLabel(cityLevelLabel, fontSize: 22, color: .white)
+        configureLabel(soldierAttackLabel, fontSize: 18, color: SKColor(red: 0.74, green: 0.86, blue: 1.0, alpha: 1.0))
+        configureLabel(cityHPLabel, fontSize: 18, color: SKColor(red: 0.88, green: 0.95, blue: 0.90, alpha: 1.0))
+        configureLabel(feedbackLabel, fontSize: 16, color: SKColor(red: 0.95, green: 0.91, blue: 0.78, alpha: 1.0))
+
+        hpBarBackground.fillColor = SKColor(red: 0.17, green: 0.19, blue: 0.22, alpha: 1.0)
+        hpBarBackground.strokeColor = SKColor(red: 0.31, green: 0.35, blue: 0.39, alpha: 1.0)
+        hpBarBackground.lineWidth = 2
+        hpBarFill.fillColor = SKColor(red: 0.16, green: 0.76, blue: 0.43, alpha: 1.0)
+        hpBarFill.strokeColor = .clear
+
+        configureButton(
+            spawnButton,
+            background: spawnButtonBackground,
+            label: spawnButtonLabel,
+            name: ButtonName.spawn,
+            color: SKColor(red: 0.12, green: 0.47, blue: 0.84, alpha: 1.0)
+        )
+        configureButton(
+            upgradeButton,
+            background: upgradeButtonBackground,
+            label: upgradeButtonLabel,
+            name: ButtonName.upgrade,
+            color: SKColor(red: 0.56, green: 0.30, blue: 0.78, alpha: 1.0)
+        )
+
+        addChild(goldLabel)
+        addChild(cityLevelLabel)
+        addChild(soldierAttackLabel)
+        addChild(cityHPLabel)
+        addChild(hpBarBackground)
+        addChild(hpBarFill)
+        addChild(feedbackLabel)
+        addChild(spawnButton)
+        addChild(upgradeButton)
     }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+
+    private func configureLabel(_ label: SKLabelNode, fontSize: CGFloat, color: SKColor) {
+        label.fontSize = fontSize
+        label.fontColor = color
+        label.horizontalAlignmentMode = .center
+        label.verticalAlignmentMode = .center
+    }
+
+    private func configureButton(
+        _ button: SKNode,
+        background: SKShapeNode,
+        label: SKLabelNode,
+        name: String,
+        color: SKColor
+    ) {
+        button.name = name
+        background.name = name
+        background.fillColor = color
+        background.strokeColor = SKColor(white: 1.0, alpha: 0.18)
+        background.lineWidth = 2
+
+        label.name = name
+        label.fontSize = 16
+        label.fontColor = .white
+        label.horizontalAlignmentMode = .center
+        label.verticalAlignmentMode = .center
+
+        button.addChild(background)
+        button.addChild(label)
+    }
+
+    private func layoutInterface() {
+        guard didBuildInterface else {
+            return
+        }
+
+        resetFontSizes()
+
+        let compactHeight = size.height < 500
+        let topMargin: CGFloat = compactHeight ? 36 : 72
+        let primaryStatusGap: CGFloat = compactHeight ? 32 : 42
+        let secondaryStatusGap: CGFloat = compactHeight ? 28 : 34
+        let hpLabelToBarGap: CGFloat = compactHeight ? 24 : 30
+        let buttonHeight: CGFloat = compactHeight ? 44 : 52
+        let buttonGap: CGFloat = compactHeight ? 10 : 12
+        let bottomMargin: CGFloat = compactHeight ? 32 : 38
+
+        let contentWidth = max(160, min(size.width - 48, 430))
+        let centerX = size.width / 2
+        let topY = size.height - topMargin
+        let upgradeButtonY = bottomMargin + buttonHeight / 2
+        let spawnButtonY = upgradeButtonY + buttonHeight + buttonGap
+        let spawnButtonTopY = spawnButtonY + buttonHeight / 2
+
+        goldLabel.position = CGPoint(x: centerX, y: topY)
+        cityLevelLabel.position = CGPoint(x: centerX, y: topY - primaryStatusGap)
+        soldierAttackLabel.position = CGPoint(x: centerX, y: cityLevelLabel.position.y - secondaryStatusGap)
+        cityHPLabel.position = CGPoint(x: centerX, y: soldierAttackLabel.position.y - secondaryStatusGap)
+
+        let hpBarSize = CGSize(width: contentWidth, height: 18)
+        hpBarBackground.path = CGPath(
+            roundedRect: CGRect(x: -hpBarSize.width / 2, y: -hpBarSize.height / 2, width: hpBarSize.width, height: hpBarSize.height),
+            cornerWidth: 9,
+            cornerHeight: 9,
+            transform: nil
+        )
+        hpBarBackground.position = CGPoint(x: centerX, y: cityHPLabel.position.y - hpLabelToBarGap)
+
+        let hpPercent = CGFloat(state.cityRemainingPower) / CGFloat(max(1, state.cityMaxPower))
+        let fillWidth = max(4, hpBarSize.width * min(max(hpPercent, 0), 1))
+        hpBarFill.path = CGPath(
+            roundedRect: CGRect(x: -hpBarSize.width / 2, y: -hpBarSize.height / 2, width: fillWidth, height: hpBarSize.height),
+            cornerWidth: 9,
+            cornerHeight: 9,
+            transform: nil
+        )
+        hpBarFill.position = hpBarBackground.position
+
+        // Keep feedback in the open band between the HP bar and action buttons.
+        let hpBarBottomY = hpBarBackground.position.y - hpBarSize.height / 2
+        let availableFeedbackGap = hpBarBottomY - spawnButtonTopY
+        let idealFeedbackY = hpBarBottomY - (compactHeight ? 30 : 46)
+        let feedbackY: CGFloat
+        if availableFeedbackGap < 64 {
+            feedbackY = spawnButtonTopY + availableFeedbackGap / 2
+        } else {
+            feedbackY = max(spawnButtonTopY + 32, min(idealFeedbackY, hpBarBottomY - 26))
+        }
+        feedbackLabel.position = CGPoint(x: centerX, y: feedbackY)
+
+        let buttonSize = CGSize(width: contentWidth, height: buttonHeight)
+        layoutButton(spawnButton, background: spawnButtonBackground, size: buttonSize, position: CGPoint(x: centerX, y: spawnButtonY))
+        layoutButton(upgradeButton, background: upgradeButtonBackground, size: buttonSize, position: CGPoint(x: centerX, y: upgradeButtonY))
+
+        fitLabel(goldLabel, maxWidth: contentWidth)
+        fitLabel(cityLevelLabel, maxWidth: contentWidth)
+        fitLabel(soldierAttackLabel, maxWidth: contentWidth)
+        fitLabel(cityHPLabel, maxWidth: contentWidth)
+        fitLabel(feedbackLabel, maxWidth: contentWidth)
+        fitLabel(spawnButtonLabel, maxWidth: contentWidth - 28)
+        fitLabel(upgradeButtonLabel, maxWidth: contentWidth - 28)
+    }
+
+    private func resetFontSizes() {
+        goldLabel.fontSize = 28
+        cityLevelLabel.fontSize = 22
+        soldierAttackLabel.fontSize = 18
+        cityHPLabel.fontSize = 18
+        feedbackLabel.fontSize = 16
+        spawnButtonLabel.fontSize = 16
+        upgradeButtonLabel.fontSize = 16
+    }
+
+    private func layoutButton(_ button: SKNode, background: SKShapeNode, size: CGSize, position: CGPoint) {
+        background.path = CGPath(
+            roundedRect: CGRect(x: -size.width / 2, y: -size.height / 2, width: size.width, height: size.height),
+            cornerWidth: 10,
+            cornerHeight: 10,
+            transform: nil
+        )
+        button.position = position
+    }
+
+    private func redraw() {
+        goldLabel.text = "Gold: \(state.gold)"
+        cityLevelLabel.text = "City Level: \(state.cityLevel)"
+        soldierAttackLabel.text = "Soldier Attack: \(state.normalSoldierAttackPower)"
+        cityHPLabel.text = "City HP: \(state.cityRemainingPower) / \(state.cityMaxPower)"
+        feedbackLabel.text = feedbackText
+        spawnButtonLabel.text = "Spawn Soldier"
+        upgradeButtonLabel.text = "Upgrade Soldier (\(state.normalSoldierUpgradeCost) gold)"
+        layoutInterface()
+    }
+
+    private func spawnSoldier() {
+        let result = state.spawnSoldierAttack()
+
+        if result.conqueredCities > 0 {
+            feedbackText = "City conquered! +\(result.goldEarned) gold."
+        } else {
+            feedbackText = "Soldier dealt \(result.damageDealt) damage."
+        }
+
+        store.save(state)
+        redraw()
+    }
+
+    private func upgradeSoldier() {
+        let result = state.upgradeNormalSoldier()
+
+        switch result {
+        case let .upgraded(cost, newAttackPower):
+            feedbackText = "Upgraded for \(cost) gold. Attack: \(newAttackPower)."
+        case let .insufficientGold(cost, currentGold):
+            feedbackText = "Need \(cost) gold. You have \(currentGold)."
+        }
+
+        store.save(state)
+        redraw()
+    }
+
+    private func buttonName(at point: CGPoint) -> String? {
+        for node in nodes(at: point) {
+            if node.name == ButtonName.spawn || node.name == ButtonName.upgrade {
+                return node.name
+            }
+        }
+
+        return nil
+    }
+
+    private func fitLabel(_ label: SKLabelNode, maxWidth: CGFloat) {
+        guard maxWidth > 0 else {
+            return
+        }
+
+        while label.frame.width > maxWidth && label.fontSize > 12 {
+            label.fontSize -= 1
+        }
     }
 }
