28603816b2e3e17d82738e0b32ee3c1e0157d0f2
cwchanap <cwchanap@connect.ust.hk>
2026-07-12 21:11:43 -0700

    feat: install approved archer animation trio

--- PyxisTests/SoldierAnimationManifestTests.swift
+++ PyxisTests/SoldierAnimationManifestTests.swift
@@ -2,10 +2,10 @@ import Testing
 @testable import Pyxis
 
 struct SoldierAnimationManifestTests {
-    @Test func currentApprovedActionsMatchInstalledArcherPilot() {
-        #expect(SoldierAnimationManifest.isAuthored(.attack, for: .archer))
-        #expect(SoldierAnimationManifest.isAuthored(.hit, for: .archer))
-        #expect(!SoldierAnimationManifest.isAuthored(.walk, for: .archer))
+    @Test func currentApprovedActionsMatchInstalledArcherTrio() {
+        #expect(SoldierAnimationAction.allCases.allSatisfy {
+            SoldierAnimationManifest.isAuthored($0, for: .archer)
+        })
         #expect(SoldierAnimationManifest.usesFullCanvas(for: .archer))
 
         for type in SoldierType.allCases where type != .archer {
